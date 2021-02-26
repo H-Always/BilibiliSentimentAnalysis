56880e1377d58d267f46443ec2a1a3e2d735a9fe
H-Always <1790199849@qq.com>
2021-02-26 13:22:02 +0800

    词云

--- BilibiliSentimentAnalysis/WordCloud.py
+++ BilibiliSentimentAnalysis/WordCloud.py
@@ -15,21 +15,21 @@ import matplotlib.pyplot as plt
 '''生成词云'''
 
 
-def word_cloud(words):
-    if not os.path.exists("Resources/word_cloud_教你如何装一台超级工作站：无敌是多么寂寞.txt"):
-        with open("Resources/word_cloud_教你如何装一台超级工作站：无敌是多么寂寞.txt", 'w', encoding='UTF-8') as file:
+def word_cloud(words, fileName):
+    if not os.path.exists("Resources/word_cloud_" + str(fileName) + ".txt"):
+        with open("Resources/word_cloud_" + str(fileName) + ".txt", 'w', encoding='UTF-8') as file:
             for word in words:
                 for i in word:
                     file.write(str(i) + ' ')
 
-    with open("Resources/word_cloud_教你如何装一台超级工作站：无敌是多么寂寞.txt", "r", encoding='UTF-8') as file:
+    with open("Resources/word_cloud_" + str(fileName) + ".txt", "r", encoding='UTF-8') as file:
         txt = file.read()
 
     mask = np.array(image.open("Resources/python_word_cloud_image.png"))
     # mask = np.array(image.open("Resources/cc.jpg"))
     word = WordCloud(background_color='black',
                      font_path='simhei.ttf',mask=mask, ).generate(txt)
-    word.to_file('Visualization/word_cloud_教你如何装一台超级工作站：无敌是多么寂寞.png')
+    word.to_file('Visualization/word_cloud_' + str(fileName) + '.png')
     print("词云图片已保存")
 
     plt.imshow(word)  # 使用plt库显示图片
@@ -40,7 +40,7 @@ def word_cloud(words):
     mask = np.array(image.open("Resources/cc.jpg"))
     word = WordCloud(background_color='black',
                      font_path='simhei.ttf', mask=mask, ).generate(txt)
-    word.to_file('Visualization/word_cloud_教你如何装一台超级工作站：无敌是多么寂寞1.png')
+    word.to_file('Visualization/word_cloud_' + str(fileName) + '1.png')
     print("词云图片已保存")
 
     plt.imshow(word)  # 使用plt库显示图片
